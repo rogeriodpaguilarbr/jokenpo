96cc8d30b1af2099cd98d6533baa2ddce732f952
rogeriodpaguilarbr <67205054+rogeriodpaguilarbr@users.noreply.github.com>
2020-06-21 20:14:10 -0300

    Removendo arquivos desnecessários

--- src/main/java/com/jokenpo/model/constantes/TiposDeJogadaEnum.java
+++
@@ -1,9 +0,0 @@
-package com.jokenpo.model.constantes;
-
-/**
- * @author Rogério de Paula Aguilar
- */
-
-public enum TiposDeJogadaEnum {
-	TESOURA, PAPEL, PEDRA, LAGARTO, SPOCK;
-}
--- src/main/java/com/jokenpo/resoure/CustomErrorType.java
+++
@@ -1,9 +0,0 @@
-package com.jokenpo.resoure;
-
-public class CustomErrorType {
-
-	public CustomErrorType(int i, String string) {
-		// TODO Auto-generated constructor stub
-	}
-
-}
